8d327586939d7fc626403067114fead67403e64e
eng-yasmine <yasmeen144@yahoo.com>
2025-11-13 16:55:38 +0200

    admin integration work flow

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="content-footer footer bg-footer-theme">
+<div
+    class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+    <div class="mb-2 mb-md-0">
+      
+        All Right Reserved made  <i class="fa fa-heart text-danger"></i> by Eng-Yasmeen Khaled   ©
+        <script>
+            document.write(new Date().getFullYear());
+        </script> .
+       
+    </div>
+  
+</div>
+</footer>
