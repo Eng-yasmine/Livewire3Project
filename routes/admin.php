7e29ec553236c95d7bda421db457d84c7ff28b23
eng-yasmine <yasmeen144@yahoo.com>
2025-11-13 18:05:57 +0200

    admin login page integration

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/admin/')->name('admin.')->group(function (){
     Route::view('dashboard',  'admin.dashboard')->name('dashboard');
+
+    Route::view('login', 'admin.auth.login')->name('login');
 });
 
 
